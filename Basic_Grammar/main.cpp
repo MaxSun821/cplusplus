c486bf843ad40a843e026415314fa9521e654955
maxsun821 <max.y.h.sun@outlook.com>
2023-09-21 21:22:46 +0800

    内联和定义宏的区别

--- Basic_Grammar/main.cpp
+++ Basic_Grammar/main.cpp
@@ -1,7 +1,45 @@
-#include <stdio.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "hello world" << endl;
+//    return 0;
+//}
+
+#include <iostream>
+// #define ADD(x,y) ((x)+(y))
+
+inline int Add(int x, int y)
+{
+    int z = x + y;
+    z = x + y;
+    z = x + y;
+    z = x + y;
+    z = x - y;
+    z -= x + y;
+    z += x + y;
+    z = x * y;
+    z = x + y;
+    z += x + y;
+    z = x / y;
+    z = x + y;
+    z = x - y;
+    z = x + y;
+
+
+    return z;
+}
+
 
 int main()
 {
-    printf("hello world\n");
+    int c = Add(3, 4);
+
+    std::cout << c << std::endl;
+
     return 0;
 }
+
+
