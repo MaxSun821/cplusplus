420987b568dfd53c3dc59853541ed71c002c3914
maxsun821 <max.y.h.sun@outlook.com>
2023-10-30 21:43:30 +0800

    一些string练习代码

--- String/main.cpp
+++ String/main.cpp
@@ -147,25 +147,159 @@ using namespace std;
 //    }
 //};
 
-class Solution {
-public:
-    string reverseStr(string s, int k) {
-        int n = s.size();
-        for(int i = 0; i < n; i += 2 * k) {
-            reverse(s.begin() + i, s.begin() + min(i + k, n));
-        }
-        return s;
+//class Solution {
+//public:
+//    string reverseStr(string s, int k) {
+//        int n = s.size();
+//        for(int i = 0; i < n; i += 2 * k) {
+//            reverse(s.begin() + i, s.begin() + min(i + k, n));
+//        }
+//        return s;
+//
+//    }
+//};
 
-    }
-};
+//int main() {
+//    string s1 = "abcdefg";
+//    string s2 = "456";
+//    Solution s;
+//    string i = s.reverseStr(s1, 2);
+//
+//    cout << i << endl;
+//
+//    return 0;
+//}
 
-int main() {
-    string s1 = "abcdefg";
-    string s2 = "456";
-    Solution s;
-    string i = s.reverseStr(s1, 2);
+//int main(int argc, char* argv[])
+//
+//{
+//
+//	string a = "hello world";
+//
+//	string b = a;
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	string c = b;
+//
+//	c = "";
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	a = "";
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	return 0;
+//
+//}
 
-    cout << i << endl;
+//int main()
+//
+//{
+//
+//    string str("Hello Bit.");
+//
+//    str.reserve(111);
+//
+//    str.resize(5);
+//
+//    str.reserve(50);
+//
+//    cout << str.size() << ":" << str.capacity() << endl;
+//
+//    return 0;
+//
+//}
 
-    return 0;
+//﻿int main()
+//{
+//
+//}
+
+int main()
+{
+	//string strText = "How are you?";
+
+	//string strSeparator = " ";
+
+	//string strResult;
+
+	//int size_pos = 0;
+
+	//int size_prev_pos = 0;
+
+	//while ((size_pos = strText.find_first_of(strSeparator, size_pos)) != string::npos)
+
+	//{
+
+	//	strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+
+	//	cout << strResult << " ";
+
+	//	size_prev_pos = ++size_pos;
+
+	//}
+
+	//if (size_prev_pos != strText.size())
+
+	//{
+
+	//	strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+
+	//	cout << strResult << " ";
+
+	//}
+
+	//cout << endl;
+
+	//return 0;
+
+	string str = "Hello Bit.";
+
+	string::iterator it = str.begin();
+
+	while (it != str.end())
+
+	{
+
+		if (*it != ' ')
+
+			cout << *it;
+
+		else
+
+			str.erase(it);
+
+		it++;
+
+	}
+
+	return 0;
 }
+
+
