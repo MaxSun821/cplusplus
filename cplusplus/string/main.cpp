162028f01ec0f313454176c40e9fc8e60d7e37f0
maxsun821 <maxsun8216@gmail.com>
2024-05-28 21:47:37 +0800

    string类的测试

--- cplusplus/string/main.cpp
+++ cplusplus/string/main.cpp
@@ -37,8 +37,22 @@ void test_string2() {
 
 }
 
+void test_string3() {
+    std::string s1 = "https://cplusplus.com/reference/string/string/?kw=string";
+    size_t i1 = s1.find(':');
+    std::string sub1, sub2, sub3;
+    sub1 = s1.substr(0, i1);
+    size_t i2 = s1.find('/', i1 + 3);
+    sub2 = s1.substr(i1 + 3, i2);
+    sub3 = s1.substr(i2 + 1);
+    std::cout << sub1 << std::endl;
+    std::cout << sub2 << std::endl;
+    std::cout << sub3 << std::endl;
+}
+
 int main() {
 	//test_string1();
-	test_string2();
+//	test_string2();
+    test_string3();
 	return 0;
 }
