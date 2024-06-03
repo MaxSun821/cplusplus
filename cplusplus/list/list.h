4b8914f1fac7c199b0677f9c3837bd49318f8297
maxsun <maxsun8216@gmail.com>
2024-06-04 07:14:17 +0800

    list class

---
+++ cplusplus/list/list.h
@@ -0,0 +1,26 @@
+
+#ifndef CPLUSPLUS_LIST_H
+#define CPLUSPLUS_LIST_H
+
+#include <iostream>
+#include <assert.h>
+#include <string>
+
+namespace max {
+  templet <class T>
+  struct _list_node {
+    typedef _list_node Node;
+    T val;
+    Node* prev;
+    Node* next;
+  };
+
+  class list {
+    private:
+      
+  }
+};
+
+
+
+#endif
