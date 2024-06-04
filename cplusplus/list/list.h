23d41e29261e03a7c0e1decc521d77d707a226b9
maxsun821 <maxsun8216@gmail.com>
2024-06-04 13:09:12 +0800

    简易实现list类

--- cplusplus/list/list.h
+++ cplusplus/list/list.h
@@ -3,24 +3,226 @@
 #define CPLUSPLUS_LIST_H
 
 #include <iostream>
-#include <assert.h>
 #include <string>
 
 namespace max {
-  templet <class T>
-  struct _list_node {
-    typedef _list_node Node;
-    T val;
-    Node* prev;
-    Node* next;
-  };
+    template<class T>
+    struct list_node {
+        T _val;
+        list_node<T> *_prev;
+        list_node<T> *_next;
 
-  class list {
+        list_node(const T &val = T())
+                : _val(val), _prev(nullptr), _next(nullptr) {}
+    };
+
+    template<class T>
+    struct __list_iterator {
+        typedef list_node<T> Node;
+        typedef __list_iterator<T> self;
+
+        Node *_node;
+
+        __list_iterator(Node *node)
+                : _node(node) {}
+
+        self &operator++() {
+            _node = _node->_next;
+            return *this;
+        }
+
+        self operator++(int) {
+            self tmp(*this);
+            _node = _node->_next;
+            return tmp;
+        }
+
+        self &operator--() {
+            _node = _node->_prev;
+            return *this;
+        }
+
+        self operator--(int) {
+            self tmp(*this);
+            _node = _node->_prev;
+            return tmp;
+        }
+
+        T &operator*() {
+            return _node->_val;
+        }
+
+        bool operator!=(const self &it) {
+            return _node != it._node;
+        }
+    };
+
+    template<class T>
+    class list {
+        typedef list_node<T> Node;
+
+    public:
+        typedef __list_iterator<T> iterator;
+
+        list() {
+            empty_init();
+        }
+
+        list(const list<T> &lt) {
+            empty_init();
+            for (auto e: lt) {
+                push_back(e);
+            }
+        }
+
+        void swap(list<T> &lt) {
+            std::swap(_head, lt._head);
+            std::swap(_size, lt._size);
+        }
+
+        list<T> &operator=(list<T> tmp) {
+            swap(tmp);
+            return *this;
+        }
+
+        void empty_init() {
+            _head = new Node;
+            _head->_prev = _head;
+            _head->_next = _head;
+        }
+
+        void push_back(const T &val) {
+//            Node *newNode = new Node(val);
+//            Node *prev = _head->_prev;
+//            prev->_next = newNode;
+//            newNode->_prev = prev;
+//            newNode->_next = _head;
+//            _head->_prev = newNode;
+//            ++_size;
+            insert(end(), val);
+        }
+
+        void push_front(const T &val = T()) {
+            insert(begin(), val);
+        }
+
+        void pop_front() {
+            erase(begin());
+        }
+
+        void pop_back() {
+            erase(--end());
+        }
+
+        iterator insert(iterator pos, const T &val = T()) {
+            Node *newNode = new Node(val);
+            Node *cur = pos._node;
+            Node *prev = cur->_prev;
+            prev->_next = newNode;
+            newNode->_prev = prev;
+            newNode->_next = cur;
+            cur->_prev = newNode;
+            ++_size;
+            return iterator(newNode);
+        }
+
+        iterator erase(iterator pos) {
+            Node *cur = pos._node;
+            Node *next = cur->_next;
+            Node *prev = cur->_prev;
+            delete cur;
+            prev->_next = next;
+            next->_prev = prev;
+            --_size;
+            return iterator(next);
+        }
+
+        iterator begin() {
+            return _head->_next;
+        }
+
+        iterator end() {
+            return _head;
+        }
+
+        ~list() {
+            clear();
+            delete _head;
+            _head = nullptr;
+        }
+
+        void clear() {
+            iterator it = begin();
+            while (it != end()) {
+                it = erase(it);
+            }
+        }
+
+        size_t size() {
+            return _size;
+        }
+
     private:
-      
-  }
-};
+        Node *_head;
+        size_t _size;
+    };
 
+    void test_list1() {
+        list<int> lt;
+        lt.push_back(1);
+        lt.push_back(2);
+        lt.push_back(3);
+        lt.push_back(4);
+        lt.push_back(5);
+
+        list<int>::iterator it = lt.begin();
+        while (it != lt.end()) {
+            std::cout << *it << " ";
+            it++;
+        }
+        std::cout << std::endl;
+
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void test_list2() {
+        list<int> lt;
+        lt.push_back(1);
+        lt.push_back(2);
+        lt.push_back(3);
+        lt.push_back(4);
+        lt.push_back(5);
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+
+        lt.insert(++lt.begin(), 90);
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+        lt.pop_back();
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+        lt.push_front(100);
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+        lt.pop_front();
+        for (auto e: lt) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+
+    }
+}
 
 
 #endif
---
+++ cplusplus/list/main.cpp
@@ -0,0 +1,8 @@
+
+#include "list.h"
+
+int main() {
+    max::test_list1();
+//    max::test_list2();
+    return 0;
+}
