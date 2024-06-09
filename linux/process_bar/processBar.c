51a6864636584e2bd214fd3994fced0fb3f8f233
maxsun <maxsun8216@gmail.com>
2024-06-09 20:43:50 +0800

    简易实现进度条

--- linux/process_bar/processBar.c
+++ linux/process_bar/processBar.c
@@ -6,11 +6,17 @@ void process_bar()
   int count = 0;
   memset(bar, '\0', sizeof(bar)); //初始化数组
 
+  const char* label = "|/-\\";
+  size_t len = strlen(label);
   while(count <= 100)
   {
-    printf("[%-100s][%-3d%%]\r", bar, count);
+    printf("[%-100s][%-3d%%][%c]\r", bar, count, label[count % len]);
     fflush(stdout);
-    bar[count++] = '=';
+    bar[count++] = STYLE;
+    if(count < 100)
+    {
+      bar[count] = '>';
+    }
     usleep(50000);
   }
   printf("\n");
--- linux/process_bar/processBar.h
+++ linux/process_bar/processBar.h
@@ -2,5 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #define NUM 102
+#define STYLE '='
+
 
 void process_bar();
