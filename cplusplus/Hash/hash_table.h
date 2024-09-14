0adb8ad1ff35339fe939f3a1ac4e9250dcef1882
maxsun821 <maxsun8216@gmail.com>
2024-09-14 13:22:52 +0800

    模拟实现哈希表

--- cplusplus/Hash/hash_table.h
+++ cplusplus/Hash/hash_table.h
@@ -8,18 +8,62 @@ namespace max
 {
 	enum Status
 	{
-
+        EMPTY,
+        EXIST,
+        DELETE
 	};
 	template<class K, class V>
 	struct HashData
 	{
 		std::pair<K, V> _data;
-
+        Status _s;
 	};
+
 	template<class K, class V>
 	class HashTable
 	{
-	private:
+    public:
+        HashTable()
+            : _n(0)
+        {
+            _v.resize(10);
+        }
+        bool insert(const std::pair<K, V> data)
+        {
+            // 扩容，设置负载因子为0.75
+            if((double)_n / _v.size() >= 0.75)
+            {
+                size_t newCapacity = _v.size() * 2;
+                HashTable<K, V> newHT;
+                newHT._v.resize(newCapacity);
 
+                for(int i = 0; i < _v.size(); i++)
+                {
+                    if(_v[i]._s == EXIST)
+                    {
+                        newHT.insert(_v[i]._data);
+                    }
+                }
+                _v.swap(newHT._v);
+            }
+
+            // 计算哈希值
+            size_t hashi = data.first % _v.size();
+            while(_v[hashi]._s == EXIST)
+            {
+                // 计算出的哈希值位置存在元素
+                // 顺序取值
+                ++hashi;
+
+                hashi %= _v.size();
+            }
+            _v[hashi]._data = data;
+            _v[hashi]._s = EXIST;
+            ++_n;
+            return true;
+        }
+	private:
+        std::vector<HashData<K, V>> _v;
+        size_t _n; // 记录哈希表中有几个元素
 	};
 }
