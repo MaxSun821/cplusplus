0702c55ac281e9e60fea1ffcbf4a0a580827373a
maxsun821 <maxsun8216@gmail.com>
2024-09-15 14:13:49 +0800

    模拟实现哈希表线性探测

--- cplusplus/Hash/main.cpp
+++ cplusplus/Hash/main.cpp
@@ -1,23 +1,52 @@
-#include <iostream>
-#include <unordered_set>
-#include <unordered_map>
+//#include <iostream>
+//#include <unordered_set>
+//#include <unordered_map>
+//
+//int main()
+//{
+//	std::unordered_set<int> set;
+//	set.insert(5);
+//	set.insert(7);
+//	set.insert(10);
+//	set.insert(2);
+//	set.insert(9);
+//
+//	std::unordered_set<int>::iterator it = set.begin();
+//	while (it != set.end())
+//	{
+//		std::cout << *it << " ";
+//		++it;
+//	}
+//	std::cout << std::endl;
+//
+//	return 0;
+//}
+
+#include "hash_table.h"
 
 int main()
 {
-	std::unordered_set<int> set;
-	set.insert(5);
-	set.insert(7);
-	set.insert(10);
-	set.insert(2);
-	set.insert(9);
+//	max::HashTable<int, int> hashTable;
+//	int arr[] = { 7, 5, 14, 4, 9, 11, 6, 36, 89, 35 };
+//
+//	for(int i = 0; i < 10; i++)
+//	{
+//		hashTable.insert(std::pair<int, int>(arr[i], arr[i]));
+//	}
+//
+//	hashTable.print();
+//	std::cout << std::endl;
+//
+//	hashTable.erase(6);
+//	hashTable.print();
+//	std::cout << std::endl;
+//
+//	hashTable.insert(std::make_pair(6, 6));
+//	hashTable.insert(std::make_pair(26, 26));
+//	hashTable.print();
+//	std::cout << std::endl;
 
-	std::unordered_set<int>::iterator it = set.begin();
-	while (it != set.end())
-	{
-		std::cout << *it << " ";
-		++it;
-	}
-	std::cout << std::endl;
 
+	max::test1();
 	return 0;
 }
