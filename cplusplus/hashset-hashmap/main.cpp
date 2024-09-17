157f3965a67aaf210f5eb7134eec2091d137a3f4
max <maxsun8216@gmail.com>
2024-09-17 12:00:11 +0800

    模拟实现位图

--- cplusplus/hashset-hashmap/main.cpp
+++ cplusplus/hashset-hashmap/main.cpp
@@ -2,11 +2,26 @@
 #include "my_unordered_map.h"
 #include "my_unordered_set.h"
 
+#include "bitset.h"
+
 int main()
 {
-	max::test_set();
-	max::test_map();
+	/*max::test_set();
+	max::test_map();*/
 
+	max::bitset<100> bs;
+	bs.set(5);
+	bs.set(7);
+	bs.set(15);
+	bs.set(66);
+	bs.set(13);
+
+	for (size_t i = 0; i < 100; i++)
+	{
+		std::cout << bs.test(i) << " ";
+	}
+
+	std::cout << std::endl;
 
 	return 0;
 }
