4e34aefb29333138ca298050cf2ad3d8372ec377
MaxSun821 <maxsun8216@gmail.com>
2024-12-21 21:48:10 +0800

    Basic c code

--- c_code/Basic/main.c
+++ c_code/Basic/main.c
@@ -1,16 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-	// sum two number
-	int num1 = 0;
-	int num2 = 0;
-	scanf("%d %d", &num1, &num2);
-	int sum = num1 + num2;
-	printf("sum = %d\n", sum);
+	printf("\a");
+	printf("\130\n");
+	_Bool flag = true;
+	printf("%d\n", flag);
 	return 0;
 }
+
+//int main()
+//{
+//	const int n = 10;
+//	int arr[n] = { 0 };
+//	return 0;
+//}
+
+//int main()
+//{
+//	// sum two number
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
 
 //int main()
 //{
