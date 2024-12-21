c6fa52615bc34f14658814977fb1f17fa0ace11a
MaxSun821 <maxsun8216@gmail.com>
2024-12-21 18:39:12 +0800

    Basic c code

---
+++ c_code/Basic/main.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	// sum two number
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	int sum = num1 + num2;
+	printf("sum = %d\n", sum);
+	return 0;
+}
+
+//int main()
+//{
+//	int age = 23;
+//	float weight = 60.3f;
+//	printf("age = %d, weight = %f\n", age, weight);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("Hello, World!\n");
+//	return 0;
+//}
