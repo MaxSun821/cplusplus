13800c6ad7688028ed9412ef94e729237b98cb3d
MaxSun821 <maxsun8216@gmail.com>
2025-01-31 22:26:29 +0800

    basic c code

--- basic/test.c
+++ basic/test.c
@@ -1046,16 +1046,216 @@
 //	return 0;
 //}
 
+//#include <stdio.h>
+//
+//extern int g_val;
+//
+//int main()
+//{
+//	
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//size_t my_strlen(const char* str)
+//{
+//	assert(str != NULL);
+//	size_t count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char str[] = "hello world";
+//	printf("strlen: %d\n", my_strlen(str));
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <assert.h>
+//char* my_strcpy(char* dest, const char* src);
+//int main()
+//{
+//	char str1[] = "hello world";
+//	char str2[] = "****************";
+//
+//	printf("%s\n", my_strcpy(str2, str1));
+//	return 0;
+//}
+//
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//
+//	char* ret = dest;
+//
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//
+//	return ret;
+//}
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//void exchange_arrays(int* arr, int size);
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	exchange_arrays(arr, 10);
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//
+//void exchange_arrays(int* arr, int size)
+//{
+//	assert(arr != NULL);
+//	int left = 0;
+//	int right = size - 1;
+//
+//	while (left < right)
+//	{
+//		while (left < right && arr[left] % 2 != 0)
+//		{
+//			++left;
+//		}
+//		while (left < right && arr[right] % 2 == 0)
+//		{
+//			--right;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//			++left;
+//			--right;
+//		}
+//	}
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int trangle[10][10] = { 0 };
+//
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (j == 0 || j == i)
+//			{
+//				trangle[i][j] = 1;
+//			}
+//			else
+//			{
+//				trangle[i][j] = trangle[i - 1][j - 1] + trangle[i - 1][j];
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%d ", trangle[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("killer: %c\n", killer);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0, b = 0, c = 0, d = 0, e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (a * b * c * d * e == 120)
+//						{
+//							if ((b == 2) + (a == 3) == 1 &&
+//								(b == 2) + (e == 4) == 1 &&
+//								(c == 1) + (d == 2) == 1 &&
+//								(c == 5) + (d == 3) == 1 &&
+//								(e == 4) + (a == 1) == 1)
+//							{
+//								printf("a = %d, b = %d, c = %d, d = %d, e = %d\n", a, b, c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	float f = 5.5f;
+//	printf("%f\n", f);
+//	return 0;
+//}
+
 #include <stdio.h>
 
-extern int g_val;
+void test(char* ptr, int arr[])
+{
+
+}
 
 int main()
 {
-	
-	printf("%d\n", g_val);
+	void (*pf)(char*, int*) = test;
+
 	return 0;
 }
-
-
-
