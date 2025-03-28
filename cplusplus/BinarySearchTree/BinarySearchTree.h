83d5d48699ba575bd033fbe82c6c41c50e67788b
MaxSun821 <maxsun8216@gmail.com>
2025-03-28 19:28:42 +0800

    二叉搜索树模拟实现，插入操作和打印操作

---
+++ cplusplus/BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,84 @@
+﻿#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+
+#include <iostream>
+
+template <typename T>
+struct TreeNode {
+    T data_;
+    TreeNode<T>* left_;
+    TreeNode<T>* right_;
+    TreeNode(const T& data)
+        : data_(data), left_(nullptr), right_(nullptr)
+    {}
+};
+
+template <typename T>
+class BinarySearchTree {
+public:
+    BinarySearchTree() : root_(nullptr) {}
+    bool insert(const T& data) {
+        if (root_ == nullptr) {
+            root_ = new TreeNode<T>(data);
+            return true;
+        }
+
+        TreeNode<T>* prev = nullptr;
+        TreeNode<T>* cur = root_;
+        while (cur != nullptr) {
+            prev = cur;
+            if (data > cur->data_) {
+                cur = cur->right_;
+            }
+            else if (data < cur->data_) {
+                cur = cur->left_;
+            }
+            else {
+                return false;
+            }
+        }
+        cur = new TreeNode<T>(data);
+        if (data < prev->data_) {
+            prev->left_ = cur;
+        }
+        else {
+            prev->right_ = cur;
+        }
+        return true;
+    }
+    bool erase(const T& data) {
+        if (root_ == nullptr) {
+            return false;
+        }
+        TreeNode<T>* cur = root_;
+        while (cur != nullptr) {
+            if (data < cur->data_) {
+                cur = cur->left_;
+            }
+            else if (data > cur->data_) {
+                cur = cur->right_;
+            }
+            else {
+                if (cur->left_ == nullptr) {
+                    // 左子树为空
+
+                }
+            }
+        }
+    }
+    void print() {
+        _print(root_);
+    }
+private:
+    void _print(TreeNode<T>* root) {
+        if (root == nullptr) {
+            return;
+        }
+        _print(root->left_);
+        std::cout << root->data_ << " ";
+        _print(root->right_);
+    }
+    TreeNode<T>* root_;
+};
+
+#endif //BINARYSEARCHTREE_H
---
+++ cplusplus/BinarySearchTree/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "BinarySearchTree.h"
+
+int main() {
+    int arr[] = {7, 5, 9, 10, 2, 3, 1, 6, 8, 4};
+    BinarySearchTree<int> tree;
+    for (int e : arr) {
+        tree.insert(e);
+    }
+    tree.print();
+    std::cout << std::endl;
+    return 0;
+}
